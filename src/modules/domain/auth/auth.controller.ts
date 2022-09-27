3dbedebbe627e9e0c863aa4aad9591b82882ced0
gvmolin <molinguin@gmail.com>
2022-09-27 11:57:46 -0300

    jwt guards

--- src/modules/domain/auth/auth.controller.ts
+++ src/modules/domain/auth/auth.controller.ts
@@ -6,16 +6,42 @@ import {
   Patch,
   Param,
   Delete,
+  Ip,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import RefreshTokenDto from './dto/refresh-token.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post()
-  authenticate(@Body() authDto: AuthDto) {
-    return this.authService.authenticate(authDto);
+  authenticate(@Req() request, @Ip() ip: string, @Body() authDto: AuthDto) {
+    return this.authService.authenticate(authDto, {
+      ipAddress: ip,
+      userAgent: request.headers['user-agent'],
+    });
+  }
+
+  @Post('refresh')
+  async refreshToken(@Body() body: RefreshTokenDto) {
+    return this.authService.refresh(body.refreshToken);
+  }
+
+  @Delete('logout')
+  async logout(@Body() body: RefreshTokenDto) {
+    return this.authService.logout(body.refreshToken);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async get() {
+    return {
+      valid: 'yess',
+    };
   }
 }
